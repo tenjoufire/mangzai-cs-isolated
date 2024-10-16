125111e42778d74bc38ae35572b028b009d54315
tenjoufire <tenjoufire7@gmail.com>
2024-10-16 06:39:36 +0000

    Implement JSON response for OpenAI Chat API in Azure Function

--- Mangzai_cs_isolated.cs
+++ Mangzai_cs_isolated.cs
@@ -18,18 +18,31 @@ namespace Company.Function
             _logger = logger;
         }
 
+        /// <summary>
+        /// Azure Functions によって呼び出されるメインメソッド
+        /// リクエスト本文をJSONで受け取り、その中にあるtextプロパティ情報を使ってOpenAI Chat APIを呼び出す
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
         [Function("Mangzai_cs_isolated")]
         public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            //リクエストBodyの抽出
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            //リクエストBodyの中にあるテキストを抽出
             string promptText = data?.text ?? string.Empty;
 
+            // Create an OpenAI client
             AzureOpenAIClient client = new(
                 new Uri(Environment.GetEnvironmentVariable("OPENAI_ENDPOINT")),
                 new DefaultAzureCredential()
             );
+
+            // Get the chat client
             ChatClient chatClient = client.GetChatClient(Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME"));
             ChatCompletion completion = await chatClient.CompleteChatAsync(
                 [
@@ -37,8 +50,11 @@ namespace Company.Function
                     new UserChatMessage(promptText)
 
                 ]);
-            //TODO: JSON response {"text": completion.Content[0].Text}
-            return new OkObjectResult(completion.Content[0].Text);
+            
+            // Return the response as JSON
+            var response = new { text = completion.Content[0].Text };
+            string jsonResponse = JsonConvert.SerializeObject(response);
+            return new OkObjectResult(jsonResponse);
         }
     }
 }
